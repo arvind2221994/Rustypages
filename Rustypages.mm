a359931f6a5cb6618baaa087bc8032428acb7400
abhishek <shaabhishek@gmail.com>
2014-06-05 21:15:05 +0900

    restarting the project. adding a mindmap file also. use freemind 1.0.1 to open

---
+++ Rustypages.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1401970224477" ID="ID_1909039276" MODIFIED="1401970234296" TEXT="Rustypages"/>
+</map>
